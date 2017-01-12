db34d7bdf5289fffa63f6bfce945552d26a9c0c3
Oliver <oliver.erben@gmail.com>
2017-01-12 14:11:45 +0100

    Create stats.php

---
+++ stats.php
@@ -0,0 +1,16 @@
+<?php
+//Datenbank Einstellungen
+$pdo = new PDO('mysql:host=localhost;dbname=databasename', 'username', 'password');
+
+//Anzahl der Personen im Stammbaum
+$statement = $pdo->prepare("SELECT COUNT(*) AS anzahl FROM wt_name WHERE n_type = ?");
+$statement->execute(array('NAME'));  
+$row = $statement->fetch();
+$statement1 = $pdo->prepare("SELECT COUNT(*) AS doppelt FROM wt_name WHERE n_surname = ?");
+$statement1->execute(array('@N.N.'));  
+$row1 = $statement1->fetch();
+$zahl1 = $row['anzahl'];
+$zahl2 = $row1['doppelt'];
+$ergebnis = $zahl1 - $zahl2;
+echo "Anzahl von Personen im Stammbaum: $ergebnis";
+?>
