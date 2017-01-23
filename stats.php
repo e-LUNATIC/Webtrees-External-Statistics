f5863d231ed83b1d4afabc280e75fa75f596334d
Oliver <oliver.erben@gmail.com>
2017-01-23 11:23:22 +0100

    added Mediacount and Bootstrap tables

--- stats.php
+++ stats.php
@@ -1,3 +1,19 @@
+<!doctype html>  
+<html lang="de">  
+<head>  
+<meta charset="utf-8">  
+<meta name="viewport" content="width=device-width, initial-scale=1">  
+<title>Webtrees Datenbankstatistiken | ollinet.org</title>  
+<meta name="description" content="Webtrees Datenbankstatistiken | ollinet.org">  
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">  
+</head>  
+<body>  
+<div class="container">  
+<div class="row">  
+<div class="col-md-12">  
+<table class='table table-hover'>  
+<tr>  
+</tr>  
 <?php
 /**
  * webtrees: online genealogy
@@ -11,14 +27,22 @@ $pdo = new PDO('mysql:host=localhost;dbname=databasename', 'username', 'password
 $statement = $pdo->prepare("SELECT COUNT(*) AS anzahl FROM wt_name WHERE n_type = ?");
 $statement->execute(array('NAME'));  
 $row = $statement->fetch();
-//Anzahl der Spitznamen
 $statement1 = $pdo->prepare("SELECT COUNT(*) AS doppelt FROM wt_name WHERE n_surname = ?");
 $statement1->execute(array('@N.N.'));  
 $row1 = $statement1->fetch();
 $zahl1 = $row['anzahl'];
 $zahl2 = $row1['doppelt'];
-//Spitznamen werden aus dem Ergebnis herausgenommen
 $personen = $zahl1 - $zahl2;
-//Translate "Anzahl von..." into your language. Total Individuals in your tree.
-echo "Anzahl von Personen im Stammbaum: $personen";
+//Mediacount
+$statement = $pdo->prepare("SELECT COUNT(*) AS anzahl FROM wt_media");
+$statement->execute();  
+$row = $statement->fetch(); { 
+//
+echo "<tr>";  
+echo "<td>" . $personen . " Personen im Stammbaum</td>";  
+echo "</tr>"; 
+echo "<tr>";
+echo "<td>" .$row['anzahl']. " Mediendateien (Bilder, Videos, Dokumente)</td>";  
+echo "</tr>";   
+}
 ?>
