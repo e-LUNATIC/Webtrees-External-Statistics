de221d7e63ebddfadfabc899300fd251348e98c2
Oliver <oliver.erben@gmail.com>
2017-01-23 11:25:34 +0100

    Update stats.php

--- stats.php
+++ stats.php
@@ -1,3 +1,12 @@
+<?php
+/**
+ * webtrees: online genealogy
+ * Copyright (C) 2016 webtrees development team
+ * Copyright (C) 2017 oliver erben https://ollinet.org>.
+ */
+//Datenbank Einstellungen
+$pdo = new PDO('mysql:host=localhost;dbname=databasename', 'username', 'password');
+
 <!doctype html>  
 <html lang="de">  
 <head>  
@@ -14,14 +23,6 @@
 <table class='table table-hover'>  
 <tr>  
 </tr>  
-<?php
-/**
- * webtrees: online genealogy
- * Copyright (C) 2016 webtrees development team
- * Copyright (C) 2017 oliver erben https://ollinet.org>.
- */
-//Datenbank Einstellungen
-$pdo = new PDO('mysql:host=localhost;dbname=databasename', 'username', 'password');
 
 //Anzahl der Personen im Stammbaum
 $statement = $pdo->prepare("SELECT COUNT(*) AS anzahl FROM wt_name WHERE n_type = ?");
