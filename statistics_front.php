2b58c423dcffa1566e474b8491623c3979f5eeb7
Oliver <oliver.erben@gmail.com>
2017-03-07 20:58:38 +0100

    remove bootstrap css
    Please uncomment the bootstrap css of your site doesn't have already bootstrap css included.
    Otherwise it breaks your design.

--- statistics_front.php
+++ statistics_front.php
@@ -5,7 +5,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">  
 <title>Webtrees Datenbankstatistiken | ollinet.org</title>  
 <meta name="description" content="Webtrees Datenbankstatistiken | ollinet.org">  
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">  
+<!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"> -->
 </head>  
 <body>  
 <div class="container">  
